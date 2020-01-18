f900b1d4f055110a5481321deaf568887a2b41c7
Раїса Кулаковська <kulakovska@rivne.itstep.org>
2020-01-18 18:37:46 +0200

    +cons

--- src/components/People/people.js
+++ src/components/People/people.js
@@ -3,30 +3,30 @@ import "./people.css";
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
 
-class People extends React.Component{
-    state={
-        PeopleList:[]
+class People extends React.Component {
+    state = {
+        PeopleList: []
     }
-    componentDidMount=()=>{
-       const url_people="https://swapi.co/api/people";
-       fetch(url_people, {metod:"GET"})
-       .then(responce=>{
-           return responce.json();
-       }) 
-       .then(results=>{
-           this.setState({
-            PeopleList: results
-           });
-       })
+    componentDidMount = () => {
+        const url_people = "https://swapi.co/api/people";
+        fetch(url_people, { metod: "GET" })
+            .then(responce => {
+                return responce.json();
+            })
+            .then(({ results }) => {
+                this.setState({
+                    PeopleList: results
+                });
+            })
     }
-    render(){
-        return(
+    render() {
+        console.log(this.state.PeopleList)
+        let { name, height } = this.state.PeopleList
+        return (
             <div className="container text-center" >
-                <Header></Header>
-                <People 
-                PeopleArr={this.state.PeopleList}
-                ></People>
-                <Footer></Footer>
+                {name}
+                {height}
+
             </div>
         )
     }
